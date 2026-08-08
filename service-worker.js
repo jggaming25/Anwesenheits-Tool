fa6242a132ea3f3a1f9e6efb17967a93a19ba770
Jann Genzmann <derdummejunge187@gmail.com>
2026-08-08 15:42:43 +0200

    Service-Worker: Cache-Version v2 (frische App-Shell nach Lockdown-Update)

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "anwesenheits-check-v1";
+const CACHE_NAME = "anwesenheits-check-v2";
 const APP_SHELL = ["index.html", "style.css", "manifest.json", "icons/icon-192.png", "icons/icon-512.png"];
 
 self.addEventListener("install", event => {
